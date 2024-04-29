a48ba0fc31a9e62e960054dafaa35b964c42b0c5
Wassamont <nene.nesaki@gmail.com>
2024-04-29 15:08:32 +0700

    my booking

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -5,15 +5,18 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { Suspense } from "react"
 import { LinearProgress } from "@mui/material"
+import getUserProfile from "@/libs/getUserProfile";
 
 export default async function ManageReservations(){
     const sessionReady = await getServerSession(authOptions) ;
     if ( !sessionReady || !sessionReady.user.token) return null
 
+    const profile = await getUserProfile(sessionReady.user.token)
+
     return(
         <main>
             <Suspense fallback={<p>Loading...<LinearProgress/></p>}>
-            <BookingList session={sessionReady}/>
+            <BookingList session={sessionReady} profile={profile}/>
             <User/>
             </Suspense>
         </main>
--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -19,12 +19,12 @@ import updateReservationStatus from "@/libs/updateReservationStatus";
 import { useRouter } from "next/navigation";
 
 
-export default function BookingList ({session}:{session:any}) {
+export default function BookingList ({session, profile}:{session:any, profile:any}) {
 
     const router = useRouter();
 
-    const [showOptions, setShowOptions] = useState(false);
-    const [profile, setProfile] = useState<any>();
+    //const [showOptions, setShowOptions] = useState(false);
+    //const [profile, setProfile] = useState<any>();
 
     async function editStatus(token: string, rid:string,reserve:Reservation,status:string){
         //console.log(token);
@@ -87,18 +87,18 @@ export default function BookingList ({session}:{session:any}) {
 
     useEffect(() => {
         data()
-        const fetchData = async () => {
-            try {
-              if (session && session.user.token){
-                const userProfile = await getUserProfile(session.user.token);
-                setProfile(userProfile);
-              } 
-            } catch (error) {
-              console.error("Error fetching data:", error);
-            }
-          };
+        // const fetchData = async () => {
+        //     try {
+        //       if (session && session.user.token){
+        //         const userProfile = await getUserProfile(session.user.token);
+        //         setProfile(userProfile);
+        //       } 
+        //     } catch (error) {
+        //       console.error("Error fetching data:", error);
+        //     }
+        //   };
       
-          fetchData();
+        //   fetchData();
     }, []);
 
     return (
@@ -203,10 +203,3 @@ export default function BookingList ({session}:{session:any}) {
         </div>
     )
 }
-
-
-
-
-
-
-
